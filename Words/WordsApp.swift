8c692694fd8987582269c0e51ba2b3b1eb5a4b05
Morgan <Mharrisstoertz@gmail.com>
2023-04-20 11:38:50 -0400

    Initial Commit

---
+++ Words/WordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordsApp.swift
+//  Words
+//
+//  Created by Morgan Harris-Stoertz on 2023-04-20.
+//
+
+import SwiftUI
+
+@main
+struct WordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
